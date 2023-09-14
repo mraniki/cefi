35b047d9e3ca4a64f0d5d9d978368378be6a1b66
mraniki <8766259+mraniki@users.noreply.github.com>
2023-09-14 15:49:15 +0200

    ♻️

--- cefi/main.py
+++ cefi/main.py
@@ -26,7 +26,7 @@ class CexTrader:
         """
 
         self.commands = settings.ccxt_commands
-        exchanges = settings.exchanges
+        exchanges = settings.exchanges.cex
         self.cex_info = []
         try:
             for exchange in exchanges:
