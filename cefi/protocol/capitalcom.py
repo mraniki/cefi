25f680629c13aeb40daf9b73d2a4e33d98b154ed
mraniki <8766259+mraniki@users.noreply.github.com>
2024-03-22 16:04:43 +0100

    ✨ new interface with capital.com broker api

---
+++ cefi/protocol/capitalcom.py
@@ -0,0 +1,171 @@
+"""
+
+Capital.com API client
+
+
+"""
+
+from capitalcom.client import Client
+from loguru import logger
+
+from .client import CexClient
+
+
+class CexCcxt(CexClient):
+    """
+    Capital.com client
+    using
+    https://pypi.org/project/capitalcom-python/
+    via Capital.com API endpoint
+    https://open-api.capital.com/
+    Args:
+        None
+
+    Returns:
+        None
+
+    """
+
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        """
+        Initialize the capital.com client
+
+        """
+        super().__init__(**kwargs)
+        self.protocol = "capitalcom"
+        self.client = Client(
+            log=self.user_id,
+            password=self.password,
+            api_key=self.api_key,
+        )
+
+    async def get_quote(self, instrument):
+        """
+        Return a quote for a instrument
+        of a given exchange ccxt object
+
+
+        Args:
+            cex
+            instrument
+
+        Returns:
+            quote
+        """
+        try:
+            instrument = await self.replace_instrument(instrument)
+
+            ticker = self.client.single_market(instrument)
+            quote = ticker["snapshot"]["offer"]
+            logger.debug("Quote: {}", quote)
+            return quote
+        except Exception as e:
+            logger.error("{} Error {}", self.name, e)
+
+    async def get_account_balance(self):
+        """
+        return account balance of
+        a given ccxt exchange
+
+        Args:
+            None
+
+        Returns:
+            balance
+
+        """
+        accounts_data = self.client.all_accounts()
+        accounts = accounts_data.get("accounts", [])
+
+        balances = [
+            f"{account['accountName']}: {account['balance']['balance']}\n"
+            for account in accounts
+            if "balance" in account and account["balance"].get("balance") is not None
+        ]
+
+        return "".join(balances)
+
+    async def get_account_position(self):
+        """
+        Return account position.
+        of a given exchange
+
+        Args:
+            None
+
+        Returns:
+            position
+
+        """
+
+        try:
+            if positions := self.client.all_positions():
+                return f"{positions}"
+        except Exception as e:
+            logger.error("{} Error {}", self.name, e)
+
+    async def pre_order_checks(self, order_params):
+        """ """
+        return True
+
+    async def get_trading_asset_balance(self):
+        """
+        Return the available balance of the trading asset from the account.
+
+        Returns:
+            float: The available balance of the trading asset.
+        """
+        accounts_data = self.client.all_accounts()
+        return next(
+            (
+                account["balance"]["available"]
+                for account in accounts_data["accounts"]
+                if account["accountName"] == self.trading_asset
+            ),
+            0,
+        )
+
+    async def execute_order(self, order_params):
+        """
+        Execute order
+
+        Args:
+            order_params (dict):
+                action(str)
+                instrument(str)
+                quantity(int)
+
+        Returns:
+            trade_confirmation(dict)
+
+        """
+        try:
+            action = order_params.get("action")
+            instrument = await self.replace_instrument(order_params.get("instrument"))
+            quantity = order_params.get("quantity", self.trading_risk_amount)
+            logger.debug("quantity {}", quantity)
+            amount = await self.get_order_amount(
+                quantity=quantity,
+                instrument=instrument,
+                is_percentage=self.trading_risk_percentage,
+            )
+
+            logger.debug("amount {}", amount)
+            pre_order_checks = await self.pre_order_checks(order_params)
+            logger.debug("pre_order_checks {}", pre_order_checks)
+
+            if amount and pre_order_checks:
+                if order := self.client.place_the_position(
+                    direction=action,
+                    epic=instrument,
+                    size=amount,
+                ):
+                    return await self.get_trade_confirmation(order, instrument, action)
+            return f"Error executing {self.name}"
+
+        except Exception as e:
+            logger.error("{} Error {}", self.name, e)
+            return f"Error executing {self.name}"
--- cefi/protocol/client.py
+++ cefi/protocol/client.py
@@ -6,6 +6,7 @@ class
 
 """
 
+# import aiohttp
 from loguru import logger
 
 
@@ -221,3 +222,37 @@ class CexClient:
         trade_confirmation += f"🗓️ {trade['datetime']}"
         if trade_confirmation:
             return f"{self.name}:\n{trade_confirmation}"
+
+    # async def fetch_url(self, url, params=None, headers=None):
+    #     """
+    #     Asynchronously gets a url payload
+    #     and returns the response.
+
+    #     Args:
+    #         url (str): The url to get.
+    #         params (dict, optional): The params to send. Defaults to None.
+    #         headers (dict, optional): The headers to send. Defaults to None.
+
+    #     Returns:
+    #         dict or None: The response or None if an
+    #         error occurs or the response is too large.
+
+    #     """
+    #     max_response_size = 10 * 1024 * 1024  # 10 MB
+    #     try:
+    #         async with aiohttp.ClientSession() as session:
+    #             async with session.get(
+    #                 url, params=params, headers=headers, timeout=20
+    #             ) as response:
+    #                 if response.status == 200:
+    #                     if (
+    #                         response.content_length
+    #                         and response.content_length > max_response_size
+    #                     ):
+    #                         logger.warning("Response content is too large.")
+    #                         return None
+    #                     return await response.json(content_type=None)
+    #                 logger.warning(f"Received non-200 status code: {response.status}")
+    #     except Exception as error:
+    #         logger.error(f"Unexpected error occurred: {error}")
+    #     return None
