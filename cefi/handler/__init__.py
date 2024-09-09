96cd547b7eae0cd1822889bb025e98d8effdee7b
mraniki <8766259+mraniki@users.noreply.github.com>
2024-09-09 13:33:48 +0200

    🥚 Basic Oanda client

--- cefi/handler/__init__.py
+++ cefi/handler/__init__.py
@@ -2,5 +2,12 @@ from .capitalcom import CapitalHandler
 from .ccxt import CcxtHandler
 from .degiro import DegiroHandler
 from .ibkr import IbHandler
+from .oanda import OandaHandler
 
-__all__ = ["CapitalHandler", "CcxtHandler", "DegiroHandler", "IbHandler"]
+__all__ = [
+    "CapitalHandler",
+    "CcxtHandler",
+    "DegiroHandler",
+    "IbHandler",
+    "OandaHandler",
+]
--- cefi/handler/oanda.py
+++ cefi/handler/oanda.py
@@ -1,100 +1,129 @@
-# """
+"""
 
-# Oanda client
-
-
-# """
+Oanda client
 
 
-# from loguru import logger
+"""
 
-# from ._client import CexClient
+from easyoanda import easyoanda
+from loguru import logger
 
-
-# class OandaHandler(CexClient):
-#     """
-#     library: https://pypi.org/project/easyoanda/
-
-#     Args:
-#         None
-
-#     Returns:
-#         None
-
-#     """
-
-#     def __init__(
-#         self,
-#         **kwargs,
-#     ):
-#         """
-#         Initialize the client
-
-#         """
-#         super().__init__(**kwargs)
+from ._client import CexClient
 
 
+class OandaHandler(CexClient):
+    """
+    library: https://pypi.org/project/easyoanda/
 
-#     async def get_quote(self, instrument):
-#         """
-#         Return a quote for a instrument
+    Args:
+        None
 
+    Returns:
+        None
 
-#         Args:
-#             cex
-#             instrument
+    """
 
-#         Returns:
-#             quote
-#         """
-#         pass
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        """
+        Initialize the client
 
-#     async def get_account_balance(self):
-#         """
-#         return account balance
+        """
+        super().__init__(**kwargs)
+        self.client = easyoanda.start_session(
+            sessionType="paper",
+            accountID=self.user_id,
+            token=self.api_key,
+        )
 
-#         Args:
-#             None
-
-#         Returns:
-#             balance
-
-#         """
-
-#         return 0
-
-#     async def get_account_position(self):
-#         """
-#         Return account position.
-#         of a given exchange
-
-#         Args:
-#             None
-
-#         Returns:
-#             position
-
-#         """
+    async def get_quote(self, instrument):
+        """
+        Return a quote for a instrument
 
 
-#         return 0
+        Args:
+            cex
+            instrument
 
-#     async def get_trading_asset_balance(self):
-#         """ """
-#         return 0
+        Returns:
+            quote
+        """
+        if quotes := self.client.pricing.start_stream(instrument):
+            quote = quotes[0]
+            return quote["bids"][0]["price"]
 
-#     async def execute_order(self, order_params):
-#         """
-#         Execute order
+    async def get_account_balance(self):
+        """
+        return account balance
 
-#         Args:
-#             order_params (dict):
-#                 action(str)
-#                 instrument(str)
-#                 quantity(int)
+        Args:
+            None
 
-#         Returns:
-#             trade_confirmation(dict)
+        Returns:
+            balance
 
-#         """
-#         pass
+        """
+        self.accounts_data = self.client.account.get_summary()
+        return self.accounts_data["account"]["balance"]
+
+    async def get_account_position(self):
+        """
+        Return account position.
+        of a given exchange
+
+        Args:
+            None
+
+        Returns:
+            position
+
+        """
+
+        position_data = self.client.positions.openPositions()
+        return position_data["positions"]
+
+    async def get_trading_asset_balance(self):
+        """ """
+        self.accounts_data = self.client.account.get_summary()
+        return self.accounts_data["account"]["balance"]
+
+    async def execute_order(self, order_params):
+        """
+        Execute order
+
+        Args:
+            order_params (dict):
+                action(str)
+                instrument(str)
+                quantity(int)
+
+        Returns:
+            trade_confirmation(dict)
+
+        """
+        action = order_params.get("action")
+        instrument = await self.replace_instrument(order_params.get("instrument"))
+        quantity = order_params.get("quantity", self.trading_risk_amount)
+        if not action or not instrument:
+            raise ValueError("Missing required parameters: action or instrument")
+        logger.debug(f"quantity {quantity}")
+        amount = await self.get_order_amount(
+            quantity=quantity,
+            instrument=instrument,
+            is_percentage=self.trading_risk_percentage,
+        )
+        if not amount:
+            raise ValueError("Failed to calculate order amount")
+        logger.debug(f"amount {amount}")
+        pre_order_checks = await self.pre_order_checks(order_params)
+        if not pre_order_checks:
+            raise ValueError("Pre-order checks failed")
+
+        if action == "SELL":
+            amount = -amount
+
+        marketOrder = easyoanda.MarketOrder()
+        marketOrder.set(instrument=instrument, units=amount)
+        self.client.orders.place_order(marketOrder)
