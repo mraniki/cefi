98d16319afb0add143e728a9b073fe74d65a0ac2
mraniki <8766259+mraniki@users.noreply.github.com>
2024-09-15 16:31:20 +0200

    🥚 ig placeholder

---
+++ cefi/handler/ig.py
@@ -0,0 +1,98 @@
+# """
+
+# ig client
+
+
+# """
+
+#
+# from loguru import logger
+
+# from ._client import CexClient
+
+
+# class IgHandler(CexClient):
+#     """
+#     library: https://github.com/ig-python/trading-ig
+
+#     Args:
+#         None
+
+#     Returns:
+#         None
+
+#     """
+
+#     def __init__(
+#         self,
+#         **kwargs,
+#     ):
+#         """
+#         Initialize the ccxt client
+
+#         """
+#         super().__init__(**kwargs)
+
+
+#     async def get_quote(self, instrument):
+#         """
+#         Return a quote for a instrument
+
+
+#         Args:
+#             cex
+#             instrument
+
+#         Returns:
+#             quote
+#         """
+#         pass
+
+#     async def get_account_balance(self):
+#         """
+#         return account balance
+
+#         Args:
+#             None
+
+#         Returns:
+#             balance
+
+#         """
+
+#         return 0
+
+#     async def get_account_position(self):
+#         """
+#         Return account position.
+
+#         Args:
+#             None
+
+#         Returns:
+#             position
+
+#         """
+
+
+#         return 0
+
+#     async def get_trading_asset_balance(self):
+#         """ """
+#         return 0
+
+#     async def execute_order(self, order_params):
+#         """
+#         Execute order
+
+#         Args:
+#             order_params (dict):
+#                 action(str)
+#                 instrument(str)
+#                 quantity(int)
+
+#         Returns:
+#             trade_confirmation(dict)
+
+#         """
+#         pass
